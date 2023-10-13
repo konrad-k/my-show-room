6bfcf60f2e87d51a44d2aba620c5956886d0768e
Konrad Kubrak <kubrakkonrad@gmail.com>
2023-10-13 13:10:05 +0200

    Fix data.json example for exibition display

--- src/pages/Exhibition.tsx
+++ src/pages/Exhibition.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import RoomGallery from 'room-gallery';
 
 const Exhibition: React.FC = () => {
-  return <RoomGallery fetch={'/data.json'} />
+  return <RoomGallery fetch={'data.json'} />
 }
 
 export default Exhibition;
